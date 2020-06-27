e5ed64b5a48b6dd83ea5481ebadc6522b278dbce
taka1127 <taka07251127@gmail.com>
2020-06-27 21:46:36 +0900

    initial commit

---
+++ src/main/java/portfolio/spring/boot/CharityLineApplication.java
@@ -0,0 +1,13 @@
+package portfolio.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityLineApplication.class, args);
+	}
+
+}
