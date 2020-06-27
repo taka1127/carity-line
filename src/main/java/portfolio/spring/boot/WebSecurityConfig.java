695dc1ab43ee807874d71349dd3736d689f1cadb
taka1127 <taka07251127@gmail.com>
2020-06-28 08:06:54 +0900

    WebSecurityConfig settings(WebSecurityConfigの設定)

---
+++ src/main/java/portfolio/spring/boot/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package portfolio.spring.boot;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//Spring Securityを使用しているため、WebサーバからCSSを取得しようとするとログインが必要になってしまう
+//WebSecurityConfigで「configure(WebSecurity)」メソッドをオーバーライドし「このパス以下はアクセス制御は必要ない（無視する）」という設定を記述
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/css/**");
+	}
+}
