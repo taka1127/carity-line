013e61ece21d7ac7656142590ff00fdcb16c5af7
taka1127 <taka07251127@gmail.com>
2020-06-28 10:18:31 +0900

    Create PersonService class(PersonServiceクラスの作成)

---
+++ src/main/java/portfolio/spring/boot/service/PersonService.java
@@ -0,0 +1,26 @@
+package portfolio.spring.boot.service;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import portfolio.spring.boot.model.Person;
+import portfolio.spring.boot.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	@Autowired
+	private PersonRepository personRepository;
+	
+	public Person createDefaultValuePerson() {
+		Person person = new Person();
+		person.setName("名前を入力してください");
+		person.setMail("please@set.email");
+		person.setTel("000-0000-0000");
+		person.setBirthday(LocalDate.of(1980, 1, 1));
+		personRepository.save(person);
+		return person;
+	}
+}
+
