50d09c4d38d64d86c4568da131c13e88aa7c28c7
taka1127 <taka07251127@gmail.com>
2020-06-28 10:14:58 +0900

    Create CompanyService class(CompanyServiceクラスを作成)

---
+++ src/main/java/portfolio/spring/boot/service/CompanyService.java
@@ -0,0 +1,25 @@
+package portfolio.spring.boot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import portfolio.spring.boot.model.Company;
+import portfolio.spring.boot.repository.CompanyRepository;
+
+@Service
+public class CompanyService {
+	
+	@Autowired
+	public CompanyRepository companyRepository;
+	
+	public Company createDefaultCompany() {
+		Company company = new Company();
+		company.setName("名前を入力してください");
+		company.setUrl("please set url");
+		company.setMail("please@set.email");
+		company.setTel("000-0000-0000");
+		companyRepository.save(company);
+		return company;
+	}
+
+}
