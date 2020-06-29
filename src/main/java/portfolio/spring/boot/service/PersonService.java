24492c218a8b87fc215a288ebf245acc1e4b54a6
taka1127 <taka07251127@gmail.com>
2020-06-29 14:10:09 +0900

    Update PersonService(PersonSeiviceの更新)

--- src/main/java/portfolio/spring/boot/service/PersonService.java
+++ src/main/java/portfolio/spring/boot/service/PersonService.java
@@ -5,7 +5,10 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import portfolio.spring.boot.model.Entry;
+import portfolio.spring.boot.model.Offer;
 import portfolio.spring.boot.model.Person;
+import portfolio.spring.boot.repository.EntryRepository;
 import portfolio.spring.boot.repository.PersonRepository;
 
 @Service
@@ -21,6 +24,32 @@ public class PersonService {
 		person.setBirthday(LocalDate.of(1980, 1, 1));
 		personRepository.save(person);
 		return person;
+	}
+	
+	@Autowired
+	private EntryRepository entryRepository;
+
+	//応募者情報を更新する
+	public void updatePerson(Person person) {
+		personRepository.save(person);
+	}
+
+	//offer, personに対応する応募情報を取得する。存在しない場合は新規のEntryオブジェクトを返す
+	public Entry getEntry(Offer offer, Person person) {
+		Entry entry = entryRepository.findByOfferAndPerson(offer, person);
+		//EntryRepositoryがnullを返したら新規Entryを作る
+		if (entry == null) {
+			entry = new Entry();
+			entry.setOffer(offer);
+			entry.setPerson(person);
+			entry.setActive(true);
+		}
+		return entry;
+	}
+
+	//応募情報を作成する
+	public Entry createEntry(Entry entry) {
+		return entryRepository.save(entry);
 	}
 }
 
