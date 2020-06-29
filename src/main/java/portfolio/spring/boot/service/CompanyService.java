7e069d1154e098e1e76c92e3e4bc7f7e25a543e6
taka1127 <taka07251127@gmail.com>
2020-06-29 09:14:58 +0900

    Update Company Service(CompanyServiceの更新)

--- src/main/java/portfolio/spring/boot/service/CompanyService.java
+++ src/main/java/portfolio/spring/boot/service/CompanyService.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import portfolio.spring.boot.model.Company;
+import portfolio.spring.boot.model.Entry;
+import portfolio.spring.boot.model.Offer;
 import portfolio.spring.boot.repository.CompanyRepository;
+import portfolio.spring.boot.repository.EntryRepository;
+import portfolio.spring.boot.repository.OfferRepository;
 
 @Service
 public class CompanyService {
@@ -20,6 +24,33 @@ public class CompanyService {
 		company.setTel("000-0000-0000");
 		companyRepository.save(company);
 		return company;
+	}
+	
+	@Autowired
+	private OfferRepository offerRepository;
+	
+	@Autowired
+	private EntryRepository entryRepository;
+	
+	//団体・企業情報を更新する
+	public void updateCompany(Company company) {
+		companyRepository.save(company);
+	}
+	
+	//募集情報を作成する
+	public Offer createOffer(Offer offer) {
+		return offerRepository.save(offer);
+	}
+	
+	//募集情報を更新する
+	public void updateOffer(Offer offer) {
+		offerRepository.save(offer);
+	}
+	
+	//応募情報を「処理済み（activeをfalse）」にする
+	public void processEntry(Entry entry) {
+		entry.setActive(false);
+		entryRepository.save(entry);
 	}
 
 }
