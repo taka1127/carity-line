8be2a831e1ab6b226eb3a53fc86623a62ba5e3df
taka1127 <taka07251127@gmail.com>
2020-06-29 13:37:04 +0900

    Create HomeService(HomeServiceの作成)

---
+++ src/main/java/portfolio/spring/boot/service/HomeService.java
@@ -0,0 +1,27 @@
+package portfolio.spring.boot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import portfolio.spring.boot.model.Offer;
+import portfolio.spring.boot.repository.OfferRepository;
+
+@Service
+public class HomeService {
+	
+	@Autowired
+	private OfferRepository offerRepository;
+	
+	//募集中の求人情報を取得する
+	public List<Offer> getOfferList(){
+		return offerRepository.findByActiveTrue();
+	}
+	
+	//求人タイトル、都道府県が指定条件に合う求人情報を検索する
+	public List<Offer> findOfferList(String word, String prefecture){
+		return offerRepository.findByActiveTrueAndTitleContainsAndPrefectureContains(word, prefecture);
+	}
+
+}
