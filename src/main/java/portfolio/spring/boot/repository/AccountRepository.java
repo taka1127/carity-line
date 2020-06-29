600ffaac56e1db0714ada506d2c59f706ca88073
taka1127 <taka07251127@gmail.com>
2020-06-29 16:02:10 +0900

    キャッシュ削除

--- src/main/java/portfolio/spring/boot/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package portfolio.spring.boot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import portfolio.spring.boot.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-
-}
